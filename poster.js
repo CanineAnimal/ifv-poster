51418712661869d51ac282bef4a4bd0cdc0bcaea
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-10-22 12:46:49 +1300

    Update poster.js

--- poster.js
+++ poster.js
@@ -16,16 +16,20 @@ function postIFV(){
 [background-block=#265780][hr][center][img]https://forum.thenorthpacific.org/images/ifv/tnp_wa_voting_footer.png[/img]
 [b][color=#dfecff]Sponsored by the[/color] [url=http://forum.thenorthpacific.org/forum/39609/][color=#94c248]Ministry of World Assembly Affairs[/color][/url] [color=#dfecff]of The North Pacific.[/color][/b][/center][hr][/background-block]`;
 	IFVtext = IFVtext.replaceAll('&', '\%26').replaceAll('?', '\%3F').replaceAll('=', '\%3D').replaceAll(';', '%3B').replaceAll('\n', '%0D%0A').replaceAll('’', '\'').replaceAll('“', '%22').replaceAll('”', '%22');
-	var request2 = new XMLHttpRequest();
-	request2.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?user_agent=Script by The Ice States (Github: https://github.com/CanineAnimal/ifv-poster) in use by ' + document.querySelector('#USER').value + '&nation=' + document.querySelector('#USER').value + '&c=dispatch&dispatch=add&title=' + dataIFV.titleRec + ' ' + dataIFV.council + ' resolution ' + dataIFV.title + '&text=' + IFVText + '&category=3&subcategory=385&mode=prepare');
-	request2.setRequestHeader('X-Password', document.querySelector('#PWD').value);
-	var originalTime = (new Date()).getTime();
-	request2.send();
-	var request3 = new XMLHttpRequest();
-	request3.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?user_agent=Script by The Ice States (Github: https://github.com/CanineAnimal/ifv-poster) in use by ' + document.querySelector('#USER').value + '&nation=' + document.querySelector('#USER').value + '&c=dispatch&dispatch=add&title=' + dataIFV.titleRec + ' ' + dataIFV.council + ' resolution ' + dataIFV.title + '&text=' + IFVText + '&category=3&subcategory=385&mode=execute&token=' + request2.responseText);
-	request3.setRequestHeader('X-Pin', request2.getResponseHeader('X-Pin'));
-	while((new Date()).getTime() < originalTime + 650){};
-	var originalTime = (new Date()).getTime();
-	request3.send();
-	console.log(request3);
+	if(confirm('Confirming that IFV ' + dataIFV.rec.toLowerCase() + ' ' + dataIFV.title + ' will be posted? Click OK below to confirm.')){
+		var request2 = new XMLHttpRequest();
+		request2.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?user_agent=Script by The Ice States (Github: https://github.com/CanineAnimal/ifv-poster) in use by ' + document.querySelector('#USER').value + '&nation=' + document.querySelector('#USER').value + '&c=dispatch&dispatch=add&title=' + dataIFV.titleRec + ' ' + dataIFV.council + ' resolution ' + dataIFV.title + '&text=' + IFVText + '&category=3&subcategory=385&mode=prepare');
+		request2.setRequestHeader('X-Password', document.querySelector('#PWD').value);
+		var originalTime = (new Date()).getTime();
+		request2.send();
+		var request3 = new XMLHttpRequest();
+		request3.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?user_agent=Script by The Ice States (Github: https://github.com/CanineAnimal/ifv-poster) in use by ' + document.querySelector('#USER').value + '&nation=' + document.querySelector('#USER').value + '&c=dispatch&dispatch=add&title=' + dataIFV.titleRec + ' ' + dataIFV.council + ' resolution ' + dataIFV.title + '&text=' + IFVText + '&category=3&subcategory=385&mode=execute&token=' + request2.responseText);
+		request3.setRequestHeader('X-Pin', request2.getResponseHeader('X-Pin'));
+		while((new Date()).getTime() < originalTime + 650){};
+		var originalTime = (new Date()).getTime();
+		request3.send();
+		console.log(request3);
+	}else{
+		alert('Cancelled IFV posting.');
+	}
 }

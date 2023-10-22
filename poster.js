c480e8bc481c54013c972738419ee8bf3324e254
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-10-22 16:19:47 +1300

    Update poster.js

--- poster.js
+++ poster.js
@@ -2,9 +2,21 @@ var dispatchID;
 var request2;
 var request3;
 var IFVtext;
+var dataIFV;
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.jsonbin.io/v3/b/6534854c0574da7622bc0472/latest', false);
+request.send(); 
+dataIFV = JSON.parse(request.responseText).record;
+
+document.querySelector('.TITLE').innerHTML = dataIFV.title;
+document.querySelector('#IFV').value = dataIFV.IFV;
+document.querySelector('#THREAD').href = dataIFV.thread;
+document.querySelector('.COUNCIL').innerHTML = dataIFV.council;
+document.querySelector('.REC').innerHTML = dataIFV.recommendation;
 
 function postIFV(){
-	var request = new XMLHttpRequest();
+	request = new XMLHttpRequest();
 	request.open('GET', 'https://api.jsonbin.io/v3/b/6534854c0574da7622bc0472/latest', false);
 	request.send(); 
 	dataIFV = JSON.parse(request.responseText).record;

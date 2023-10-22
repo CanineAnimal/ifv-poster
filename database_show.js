9aa958aecc5a3dc2c12a6f738d76da9dbc9d5d90
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-10-23 09:01:51 +1300

    Create database_show.js

---
+++ database_show.js
@@ -0,0 +1,11 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.jsonbin.io/v3/b/6534854c0574da7622bc0472/latest', false);
+request.send(); 
+dataIFV = JSON.parse(request.responseText).record;
+
+document.querySelector('.TITLE').innerHTML = dataIFV.title;
+document.querySelector('#IFV').value = dataIFV.IFV;
+document.querySelector('#THREAD').href = dataIFV.thread;
+document.querySelector('#THREAD').innerHTML = 'Link';
+document.querySelector('.COUNCIL').innerHTML = dataIFV.council;
+document.querySelector('.REC').innerHTML = dataIFV.rec;

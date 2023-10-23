db77828a27934d48b0fb4abe3ef52a7189748149
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-10-23 15:12:49 +1300

    Update poster.js

--- poster.js
+++ poster.js
@@ -22,7 +22,7 @@ function postIFV(){
 		request2.setRequestHeader('X-Password', document.querySelector('#PWD').value);
 		var originalTime = (new Date()).getTime();
 		request2.send();
-		if(request2.status != 200){
+		if(request2.status == 200){
 			request3 = new XMLHttpRequest();
 			request3.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?user_agent=Script by The Ice States (Github: https://github.com/CanineAnimal/ifv-poster) in use by ' + document.querySelector('#USER').value + '&nation=' + document.querySelector('#USER').value + '&c=dispatch&dispatch=add&title=' + dataIFV.titleRec + ' ' + dataIFV.council + ' resolution ' + dataIFV.title + '&text=' + IFVtext + '&category=3&subcategory=385&mode=execute&token=' + request2.responseXML.querySelector('SUCCESS').innerHTML, false);
 			request3.setRequestHeader('X-Pin', request2.getResponseHeader('X-Pin'));
